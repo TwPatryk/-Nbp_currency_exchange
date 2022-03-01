1342c252557bb0340474180c932406a1f511f38c
TwPatryk <tw.patryk@gmail.com>
2022-03-01 17:24:24 +0100

    init

---
+++ src/main/java/pl/nbp/exchange/configuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package pl.nbp.exchange.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.nbp.exchange")
+public class AppConfiguration {
+}
